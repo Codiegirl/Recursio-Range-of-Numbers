5a12b3401c6fb885850bd6f15e685b464a91be82
github username <github email address>
2019-12-02 14:35:54 -0600

    added countdown function

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ function rangeOfNumbers(startNum, endNum) {
       return numbers
     }
   };
+
+
+
+
+  //Only change code below this line
+function countdown(myArray, n){
+    if (n <= 0){
+      return [n]
+    } else {
+      myArray.push(n)
+      countdown(myArray, n - 1)
+      
+      
+    }
+    
+  }
